ad31ee3913c53937257fca6dcaf814c35bd9ce92
Kodai Kinjo <info.aokabin@gmail.com>
2015-11-28 20:53:05 +0900

    fixed coincontroller

--- Assets/Kodai/Scene/DebugPlayerController.cs
+++ Assets/Kodai/Scene/DebugPlayerController.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class DebugPlayerController : MonoBehaviour {
 
+	public float power = 1.0f;
+	public float score = 1.0f;
+
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
@@ -13,11 +16,25 @@ public class DebugPlayerController : MonoBehaviour {
 		if (Input.GetKey ("up")) {
 			transform.position += transform.forward * 0.1f;
 		}
+		if (Input.GetKey ("down")) {
+			transform.position += transform.forward * -0.1f;
+		}
 		if (Input.GetKey("right")) {
 			transform.position += transform.right * 0.1f;
 		}
 		if (Input.GetKey ("left")) {
 			transform.position += transform.right * -0.1f;
 		}
+
+	}
+
+	public void PowerUP() {
+		Debug.Log("Power UP!!");
+		power *= 2.0f;
+	}
+
+	public void ScoreUP() {
+		Debug.Log("Score UP!!");
+		score *= 1.1f;
 	}
 }
