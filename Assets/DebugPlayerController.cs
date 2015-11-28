33eaa56fcb739ba484241f7c91432670508dfc3b
Kodai Kinjo <info.aokabin@gmail.com>
2015-11-28 17:52:20 +0900

    Create RingController

---
+++ Assets/DebugPlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugPlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("up")) {
+			transform.position += transform.forward * 0.1f;
+		}
+		if (Input.GetKey("right")) {
+			transform.position += transform.right * 0.1f;
+		}
+		if (Input.GetKey ("left")) {
+			transform.position += transform.right * -0.1f;
+		}
+	}
+}
